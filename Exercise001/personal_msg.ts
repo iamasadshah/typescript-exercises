92f1e181938ea496c53a5bd0f647a4beb41b1a72
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-03-12 01:57:27 -0700

    exercise 1

--- Exercise001/personal_msg.ts
+++ Exercise001/personal_msg.ts
@@ -3,5 +3,5 @@ let personName= prompt("What is Your name?")
 if (personName !== null && personName !== ""){
     alert(`Hello ${personName} Would You like to learn some TypeScript Today?`)
 }else{
-    alert(`Please Correct Your Name!`)
+    alert(`Correct Your Name!`)
 }
