fac0140f31205a9821e53b62f120d377c06b7576
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-03-12 01:27:07 -0700

    updated exercises

---
+++ Exercise001/personal_msg.js
@@ -0,0 +1,7 @@
+var personName = prompt("What is Your name?");
+if (personName !== null && personName !== "") {
+    alert("Hello ".concat(personName, " Would You like to learn some TypeScript Today?"));
+}
+else {
+    alert("Please Correct Your Name!");
+}
