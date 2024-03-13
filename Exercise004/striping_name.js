b5e91cf9eecb1073ff076b231a9c0420b8a8e4fa
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-03-13 12:18:37 +0500

    name striping

---
+++ Exercise004/striping_name.js
@@ -0,0 +1,5 @@
+"use strict";
+let userName = "     \n \t Asad Shah";
+console.log(userName);
+let nameStriping = userName.trim();
+console.log(nameStriping);
---
+++ Exercise004/striping_name.ts
@@ -0,0 +1,5 @@
+let userName:string = "     \n \t Asad Shah"
+console.log(userName)
+
+let nameStriping:string = userName.trim();
+console.log(nameStriping)
