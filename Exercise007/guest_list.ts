1bab7d9b48f4841ff2ff288d108906f857feb8e0
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-03-14 11:57:43 +0500

    guest list

---
+++ Exercise007/guest_list.ts
@@ -0,0 +1,14 @@
+let guestList = ["Shah", "Khan", "Syed",]
+
+// Inviting One By One
+console.log(`${guestList[0]} You Are Invited to dinner`)
+console.log(`${guestList[1]} You Are Invited to dinner`)
+console.log(`${guestList[2]} You Are Invited to dinner`)
+
+// Invite Using Loop
+for (let i=0; i<guestList.length; i++){
+    console.log(`${guestList[i]} You are Invited to dinner`)
+}
+
+// Changing List
+
