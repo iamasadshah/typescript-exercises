dd9d39503a34931d2fe9b85d0af81748caeaf40a
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-03-14 12:31:38 +0500

    guest list

--- Exercise007/guest_list.ts
+++ Exercise007/guest_list.ts
@@ -1,14 +1,37 @@
 let guestList = ["Shah", "Khan", "Syed",]
-
+const message:string = "You are invited to dinner"
 // Inviting One By One
-console.log(`${guestList[0]} You Are Invited to dinner`)
-console.log(`${guestList[1]} You Are Invited to dinner`)
-console.log(`${guestList[2]} You Are Invited to dinner`)
+console.log(`${guestList[0]} ${message}`)
+console.log(`${guestList[1]} ${message}`)
+console.log(`${guestList[2]} ${message}`)
 
 // Invite Using Loop
 for (let i=0; i<guestList.length; i++){
-    console.log(`${guestList[i]} You are Invited to dinner`)
+    console.log(`${guestList[i]} ${message}`)
 }
 
 // Changing List
+let absentGuest:string = "Syed"
+let newGuest:string = "Ali"
+
+guestList[2] = newGuest;
+for (let i=0; i<guestList.length; i++){
+    console.log(`${guestList[i]} ${message}`)
+}
+
+console.log(`${absentGuest} is not coming.`)
+
+
+//Shrinking Guest List 
+console.log("Good News! We found big table so we can invite three more people")
+
+guestList.unshift('Zia Khan')
+guestList.splice(2, 0, 'Saad')
+guestList.push("Sajid Khan")
+
+for (let i=0; i<guestList.length; i++){
+    console.log(`${guestList[i]} ${message}`)
+}
+
+
 
